79407f06934b456003c4ff17e1277010192b4bd0
Ankit Kumain <kumankit10@gmail.com>
2021-09-16 12:04:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hands Emojis Interpreter</h1>
+      <input></input>
+    </div>
+  );
+}
